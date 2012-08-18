331407d6551b523e4c51413ba22381cf6bf62ca9
Fronx <fronx@soundcloud.com>
2012-08-18 16:11:55 +0200

    refactored, not working yet.

--- text_to_midi.node.js
+++ text_to_midi.node.js
@@ -1,11 +1,39 @@
 var fs   = require('fs');
 var Midi = require('jsmidgen');
 
-var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseLetters = lowerCaseLetters.toUpperCase();
-var vowels           = "aeiou";
+TextUtils = {}
+TextUtils.punctuation      = ".!?,"
+TextUtils.lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+TextUtils.upperCaseLetters = TextUtils.lowerCaseLetters.toUpperCase();
+TextUtils.vowels           = "aeiou";
+TextUtils.lettersByFrequency = [
+  "e", "t", "a", "o", "i", "n", "s", "h",
+  "r", "d", "l", "c", "u", "m", "w", "f",
+  "g", "y", "p", "b", "v", "k", "j", "x",
+  "q", "z"
+];
+TextUtils.isLetter = function (c) {
+  return TextUtils.lowerCaseLetters.indexOf(c.toLowerCase()) > -1;
+}
+TextUtils.isLowerCase = function (c) {
+  return TextUtils.lowerCaseLetters.indexOf(c) > -1;
+}
+TextUtils.isUpperCase = function (c) {
+  return TextUtils.lowerCaseLetters.indexOf(c) > -1;
+}
+TextUtils.isVowel = function (c) {
+  return TextUtils.vowels.indexOf(c.toLowerCase()) > -1;
+}
+TextUtils.isPunctuation = function (c) {
+  return TextUtils.punctuation.indexOf(c) > -1;
+}
+TextUtils.characterIndex = function (c) {
+  return TextUtils.lowerCaseLetters.indexOf(c.toLowerCase());
+}
 
-var notes = [
+
+Music = {}
+Music.notes = [
   'c2', 'd2', 'e2', 'f2', 'g2', 'a2', 'b2',
   'c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3',
   'c4', 'd4', 'e4', 'f4', 'g4', 'a4', 'b4',
@@ -13,8 +41,7 @@ var notes = [
   'c6', 'd6', 'e6', 'f6', 'g6', 'a6', 'b6',
   'c7', 'd7', 'e7', 'f7', 'g7', 'a7', 'b7',
 ];
-
-var pentatonicNotes = [
+Music.pentatonicNotes = [
   'c1', 'd1', 'e1', 'g1', 'a1',
   'c2', 'd2', 'e2', 'g2', 'a2',
   'c3', 'd3', 'e3', 'g3', 'a3',
@@ -22,15 +49,7 @@ var pentatonicNotes = [
   'c5', 'd5', 'e5', 'g5', 'a5',
   'c6', 'd6', 'e6', 'g6', 'a6',
 ]
-
-var lettersByFrequency = [
-  "e", "t", "a", "o", "i", "n", "s", "h",
-  "r", "d", "l", "c", "u", "m", "w", "f",
-  "g", "y", "p", "b", "v", "k", "j", "x",
-  "q", "z"
-];
-
-var drumNotes = {
+Music.drumNotes = {
   kick:    35,
   snare:   38,
   hat:     42,
@@ -39,85 +58,144 @@ var drumNotes = {
   highTom: 48
 };
 
-var characterToNote = function (c, scale) {
-  var offset = lowerCaseLetters.indexOf(c);
-  return scale[offset] || 'pause';
+Lexer = {}
+Lexer.getTokens = function (text) {
+  return text.split('').map(function(c) {
+    return {
+      character:      c,
+      characterIndex: TextUtils.characterIndex(c),
+      isLetter:       TextUtils.isLetter(c),
+      isLowerCase:    TextUtils.isLowerCase(c),
+      isUpperCase:    TextUtils.isUpperCase(c),
+      isVowel:        TextUtils.isVowel(c),
+      isPunctuation:  TextUtils.isPunctuation(c),
+      isTag:          c == '#',
+      isMention:      c == '@',
+      isWhitespace:   c == ' ',
+    };
+  });
 }
 
-var characterToDrumNote = function (c) {
-  if (
-    (upperCaseLetters.indexOf(c) != -1) ||
-    (c == '!') || (c == '.') || (c == '?')
-  ) {
-    return drumNotes.kick;
-  }
-  if (vowels.indexOf(c.toLowerCase()) != -1) {
-    return drumNotes[
-      {
-        e: 'hat',
-        a: 'ride',
-        i: 'snare',
-        o: 'hatOpen',
-        u: 'highTom'
-      }[c.toLowerCase()]
-    ];
-  }
-  return null;
-}
-
-var velocityFromLetterFrequency = function(letter) {
-  return 20 + lettersByFrequency.indexOf(letter) * 3;
-}
-
-var pause = function(track, duration) {
-  track.addNoteOff(0, 'c0', duration || 64);
-}
+var maxLength = function(arrays) {
+  var max = 0;
+  arrays.forEach(function (array) {
+    if (array.length > max) {
+      max = array.length;
+    };
+  });
+  return max;
+};
 
 TextToMidi = {};
-TextToMidi.drums = {};
-TextToMidi.default = TextToMidi.drums;
-TextToMidi.drums.apply = function(text) {
-  var file   = new Midi.File();
-  var melody = new Midi.Track();
-  var drums  = new Midi.Track();
-  file.addTrack(melody);
-  file.addTrack(drums);
+TextToMidi.addNote = function(time, track, pitch, duration, velocity) {
+  track[time] = {
+    on:       true,
+    pitch:    pitch,
+    velocity: velocity,
+  };
+  track[time + duration] = {
+    on:       false,
+    pitch:    pitch,
+  };
+};
 
-  for (var i = 0; i < text.length; i++) {
-    var c        = text.toLowerCase().charAt(i);
-    var pitch    = characterToNote(c, pentatonicNotes);
-    var velocity = velocityFromLetterFrequency(c);
-    var drumNote = characterToDrumNote(text.charAt(i));
-
-    if (pitch == 'pause') {
-      pause(melody);
-      pause(drums);
-    } else {
-      melody.addNoteOn(0, pitch, 0, velocity);
-      if (drumNote != null) {
-        drums.addNoteOn(0, drumNote, 0, velocity * 3);
-        drums.addNoteOff(0, drumNote, 4);
-        melody.addNoteOff(0, pitch, 4);
-      } else {
-        // melody.addNoteOff(0, pitch, 0);
+TextToMidi.toMidi = function (tracks) {
+  var file = new Midi.File();
+  tracks.forEach(function(track) {
+    track.midiTrack = new Midi.Track();
+    file.addTrack(track.midiTrack);
+  });
+  maxTime = maxLength(tracks);
+  for (midiTime = 0; midiTime < maxTime; midiTime += 1) {
+    var anyNote = false;
+    tracks.forEach(function(track, trackIndex) {
+      var note = track[midiTime];
+      if (note != undefined) {
+        if (note.on) {
+          track.midiTrack.addNoteOn(0, note.pitch, 0, note.velocity);
+        } else {
+          track.midiTrack.addNoteOff(0, note.pitch);
+        }
+        anyNote = true;
       }
+    });
+    if (anyNote == false) {
+      // advance in time by one
+      tracks.forEach(function(track, trackIndex) {
+        track.midiTrack.addNoteOff(0, 'c0', 1);
+      });
     }
-  }
-
+  };
   return file.toBytes();
 };
+
+TextToMidi.drums = {};
+TextToMidi.drums.apply = function(text) {
+  var tracks = {
+    drums:  [],
+    melody: [],
+  };
+  var time = 0;
+  Lexer.getTokens(text).forEach(function(token) {
+    if (token.isWhitespace) {
+      time += 64;
+    } else {
+      if (token.isUpperCase || token.isPunctuation) {
+        TextToMidi.addNote(
+          time,
+          tracks.drums,
+          Music.drumNotes.kick,
+          4,
+          20 + TextUtils.lettersByFrequency.indexOf(token.character) * 5
+        )
+      };
+      if (token.isVowel) {
+        TextToMidi.addNote(
+          time,
+          tracks.drums,
+          Music.drumNotes[
+            {
+              e: 'hat',
+              a: 'ride',
+              i: 'snare',
+              o: 'hatOpen',
+              u: 'highTom'
+            }[token.character.toLowerCase()]
+          ],
+          4,
+          20 + TextUtils.lettersByFrequency.indexOf(token.character) * 5
+        );
+      };
+      if (token.isLetter) {
+        TextToMidi.addNote(
+          time,
+          tracks.melody,
+          Music.pentatonicNotes[token.characterIndex],
+          4,
+          20 + TextUtils.lettersByFrequency.indexOf(token.character) * 3
+        )
+      };
+      // advance global time
+      time += 4;
+    };
+  });
+  var indexedTracks = [];
+  indexedTracks[1]  = tracks.melody;
+  indexedTracks[10] = tracks.drums;
+  return indexedTracks;
+};
+TextToMidi.default = TextToMidi.drums;
 
 // ------------
 
 Mode = process.env.mode || 'default';
 
-if (process.env.mode == 'sandbox') {
-
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', function(text) {
-    out = TextToMidi[Mode].apply(text)
-    process.stdout.write(out, 'binary');
-  });
-}
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(text) {
+  var tracks = TextToMidi[Mode].apply(text);
+  process.stdout.write(JSON.stringify(tracks));
+  // process.stdout.write(
+  //   TextToMidi.toMidi(tracks), 'binary'
+  // );
+});
